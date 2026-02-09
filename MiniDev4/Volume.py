ba7584f6b180b6aa73e2058d948ce7d62189932f
ArchMeowster <philcler04@gmail.com>
2026-02-09 02:12:17 -0500

    v

--- MiniDev4/Volume.py
+++ MiniDev4/Volume.py
@@ -24,13 +24,15 @@ V = np.zeros((ND, NNtot+1))  # Volumes calculés pour chaque série d'essais
 Vlist = V.tolist()
 for i in np.arange(0, 4):
     Vlist[i][0] = [D_val[i]," Dimensions"]
-
-
+V3 = []
+V6 = []
+D = (np.arange(3, 7, 1))
+Vth = np.pi ** (D/2)/(sp.special.gamma(D/2+1)) # Volume théorique
 for d in range(0, ND):
     D = D_val[d]  # Dimension
     Vtot = 2 ** D # Volume du domaine
-    Vth = np.pi ** (D/2)/(sp.special.gamma(D/2+1)) # Volume théorique
-    VDth.append(Vth)
+
+    VDth = Vth
     for n in range(0, NNtot):
         Ntot = Ntot_val[n]  # Nombre de points
 
@@ -41,22 +43,36 @@ for d in range(0, ND):
             # Génération des nombres aléatoires (distribution uniforme)
             np.random.seed() # Initialise le générateur de nombres pseudo-aléatoires afin de ne pas toujours produire la même séquence à l'ouverture de Python...
             pts = a * np.random.uniform(low=-1, high=1, size=(Ntot, D)) # Coordonnées des points
-            Nint = 0
 
-            for x in pts:
-                Apts=0
-                for i in np.arange(0, D, 1):
-            # Calcul du volume
-                    Apts += x[i]**2
-
-                if Apts <= 1:
-                    Nint += 1 # Nombre de points à l'intérieur
-            
+            Nint = np.sum(np.sum(pts**2, axis=1) <= 1) 
             
             Vind[k] = Nint / Ntot * Vtot # Volume calculé pour cet essai
-        ecart_type = np.abs( - Vth)
+        ecart_type = np.std(Vind, ddof=1)
         incertitude_relative = ecart_type * 100 /(Ness**0.5 * np.mean(Vind))
         Vlist[d][n+1] = [np.mean(Vind), incertitude_relative] # Volume moyenné sur l'ensemble des essais
+        if D == 3:
+            V3.append(np.mean(Vind))
+        if D == 6:
+            V6.append(np.mean(Vind))
+
+erreur3D = np.abs(np.array(V3)-Vth[0])/Vth[0]
+erreur6D = np.abs(np.array(V6)-Vth[3])/Vth[3]
+print(erreur3D)
+print(erreur6D)
+logN = np.log(Ntot_val)
+logE3 = np.log(erreur3D)
+logE6 = np.log(erreur6D)
+
+# Ajustement linéaire
+p3, b3 = np.polyfit(logN, logE3, 1)
+p6, b6 = np.polyfit(logN, logE6, 1)
+
+print(f"Exposant p pour D=3  : {p3:.3f}")
+print(f"Exposant p pour D=6  : {p6:.3f}")
+
+plt.loglog(np.array(Ntot_val), erreur3D, 'o-', label=f"D=3")
+plt.loglog(np.array(Ntot_val), erreur6D, 's-', label=f"D=6")
+
 header = []
 header.append("Dimensions")
 for i in Ntot_val:
@@ -78,3 +94,9 @@ for i in Table:
     j+=1
 
 print(tabulate(Table, headers = header, tablefmt="github"))
+
+plt.xlabel("Ntot")
+plt.ylabel("Erreur théorique")
+plt.legend()
+plt.grid()
+plt.show()
